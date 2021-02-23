1113dd338dbb0969d03738461d430f328d49f69a
Daniel Epstein <epstein@ics.uci.edu>
2021-02-23 12:17:56 -0800

    Draft of A5 and future lectures

--- src/app/a5/a5.component.ts
+++ src/app/a5/a5.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-a5',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class A5Component implements OnInit {
 
-  constructor() { }
+  assignment:{};
 
-  ngOnInit(): void {
+  constructor(private http:HttpClient) {
+  	this.http.get('./assets/calendar.json').subscribe(calendar => {
+  		this.parseAssignment(calendar as {}, 'A5');
+  	});
+  }
+
+  ngOnInit() {
+  }
+
+  parseAssignment(calendar:{}, assignment:string) {
+    let events:any[] = calendar['events'];
+
+    //Filter to this assignment
+    this.assignment = events.filter(e => e['type'] == 'assignment' && e['title'] == assignment)[0];
+    
+    //Set due date
+	let due = moment(this.assignment['date'] + " " + calendar['defaults'].assignment.due);
+    this.assignment['due'] = due.format('dddd, MMMM Do, h:mma');
   }
 
 }
